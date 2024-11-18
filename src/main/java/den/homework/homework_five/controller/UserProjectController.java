761dde989825d5480b589a865b64d424ac8123dc
denis <denis13071981@gmail.com>
2024-11-18 23:29:03 +0300

    some fix path

--- src/main/java/den/homework/homework_five/controller/UserProjectController.java
+++ src/main/java/den/homework/homework_five/controller/UserProjectController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/user-project")
 public class UserProjectController {
 
     @Autowired
